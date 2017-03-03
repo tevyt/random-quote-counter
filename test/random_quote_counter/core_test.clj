b8c12361b3b54328c2372d1e067767f8630b3677
Travis Smith <ttevyt@gmail.com>
2017-03-03 14:47:22 +0000

    Waits till all futures are completed

---
+++ test/random_quote_counter/core_test.clj
@@ -0,0 +1,7 @@
+(ns random-quote-counter.core-test
+  (:require [clojure.test :refer :all]
+            [random-quote-counter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
